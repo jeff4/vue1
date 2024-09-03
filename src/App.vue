206a2ce94389bf3a0647e685c3821bd8b2c17d7c
Jeff Hwang <jeffh@champ-1524.local>
2024-09-02 23:21:28 -0700

    more App.vue updates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <script lang="ts">
+
   export default {
     data() {
       return{
         name: 'Kevin Chang',
         status: false,
+        tasks: ['Task One', 'Task Two', 'Task Three']
       };
     },
   };
 
 </script>
 
+
+<!--- HTML under here -->
+
 <template>
   <h1>{{ name }}</h1>
   <p v-if="status">Member is active</p>
-  <p v-else>Member is inactive</p>
+  <p v-else><em>Inactive member</em></p>
+
+  <br> <br> 
+  <h3>Tasks:</h3>
+
+
+
+
 </template>
+
+
+
+<!--- Styles under here -->
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
@@ -22,10 +38,11 @@
   * {
     font-family: "Rubik", sans-serif;
     margin: 0;
-    padding: 0;
+    padding: 5px;
     box-sizing: border-box;
   }
   
+
   .container {
     max-width: 1100px;
     margin: 0 auto;
